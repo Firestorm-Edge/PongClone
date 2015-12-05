84f6f39e828bb755240916cb7e1489b6b397acdb
Chris <chris.sinquefield@angryhangar.com>
2015-12-04 19:41:28 -0500

    GameActors and Loops h files mostly done. Need to figure out how to tell Input that it's expecting an event.

--- PongClones/PongClones/Loops.h
+++ PongClones/PongClones/Loops.h
@@ -1,1 +1,19 @@
 #pragma once
+
+class GameLoop
+{
+public:
+	GameLoop();
+	void Update();
+	void Draw();
+	void Input();
+};
+
+class MenuLoop
+{
+public:
+	MenuLoop();
+	void Update();
+	void Draw();
+	void Input();
+};
